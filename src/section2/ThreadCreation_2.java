64f3762ff4d13db06098517f2baebf1d32a862af
Abdul Basit <abdulbasit10397@gmail.com>
2023-11-22 22:50:10 +0500

    JMC-04 Completed thread exercise

--- src/section2/ThreadCreation_2.java
+++ src/section2/ThreadCreation_2.java
@@ -1,4 +1,17 @@
 package section2;
 
+
 public class ThreadCreation_2 {
+    private static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello from: " + this.getName());
+        }
+    }
+
+    public static void main (String[] args) {
+        Thread thread = new MyThread();
+        thread.setName("My Thread");
+        thread.start();
+    }
 }
