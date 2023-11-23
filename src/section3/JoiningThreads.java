bc1c0d9415a105c6b11dff03747664ec168cfdbf
Abdul Basit <abdulbasit10397@gmail.com>
2023-11-23 22:11:42 +0500

    JMC-06 Joining threads starting

---
+++ src/section3/JoiningThreads.java
@@ -0,0 +1,11 @@
+package section3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class JoiningThreads {
+    public static void main (String[] args) {
+        List<Long> numbers = Arrays.asList(0L, 3435L, 35435L, 2324L, 4656L, 23L, 2435L, 5566l);
+
+    }
+}
